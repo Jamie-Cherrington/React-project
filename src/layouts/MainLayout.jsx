fe4c489605c032db20393d87d9ad911216a2f5eb
Jamie Cherrington <jamiecherrington@googlemail.com>
2025-06-16 01:04:20 +0100

    Development progress
    Added a 404 page not found, json server, better routing and cleaner code.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar' // imported navbar so its across the whole web application 
 
 const MainLayout = () => {
-  return  <div>MainLayout</div>
+  return  <>
+   <Navbar /> 
+   <Outlet />
+  </>;
   
 };
 
